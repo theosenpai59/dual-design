3a3a386f6fee1c0b134be28e9ecb6b5da63f8bbc
theosenpai59 <theo.delaudedu59@gmail.com>
2026-06-04 00:47:18 +0200

    fix the css issue and the buffer issue

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,6 +4,9 @@ import { notFound } from 'next/navigation';
 import { routing } from '@/src/i18n/routing';
 import '../globals.css';
 
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
 export default async function LocaleLayout({
   children,
   params
